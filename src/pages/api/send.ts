3e801c9952700497ac3f5a99af457e488760e64a
bhlox <kurkboard@gmail.com>
2023-12-06 10:04:36 +0800

    api logs

--- src/pages/api/send.ts
+++ src/pages/api/send.ts
@@ -5,13 +5,15 @@ export default async function Handler(
 ) {
   console.log(req.body);
   try {
-    await fetch("https://fb-test-dedup.vercel.app/ping", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(req.body),
-    });
+    await fetch("https://fb-test-dedup.vercel.app/log", 
+    // {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(req.body),
+    // }
+    );
     return res.status(200).send("data received");
   } catch (error) {
     console.log(error);
