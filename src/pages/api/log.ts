2587f1a60abfb74d51646ba650f8c256f771dccf
bhlox <kurkboard@gmail.com>
2023-12-06 09:33:52 +0800

    tests

---
+++ src/pages/api/log.ts
@@ -0,0 +1,14 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+// import NextCors from "nextjs-cors";
+export default async function Handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+//   await NextCors(req, res, {
+//     methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+//     origin: "*",
+//     optionsSuccessStatus: 200,
+//   });
+  console.log(req.body);
+  return res.status(200).send("data received");
+}
