2b5d88e8c86dc55a37de4fe3be70d7c36fdee41c
Kurk Villanueva <kurkboard@gmail.com>
2024-02-09 18:44:17 +0800

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,7 @@ import type { NextRequest } from "next/server";
 import { sha256 } from "js-sha256";
 
 export function middleware(req: NextRequest) {
+  console.log({country:req.geo?.country});
   const userLocation = {
     ip: req.ip ?? process.env.DEVELOPER_IP ?? "unknown ip",
     city: sha256(req.geo?.city ?? process.env.DEVELOPER_CITY ?? "unknown city"),
